155f199476e31b6245d3ad41ff8fee2395f0f7ba
Onyeriri Victor <viktorchinonso@gmail.com>
2023-08-05 08:02:24 +0100

    Completed chapter 01 modules and file system

---
+++ chapter-01/math.js
@@ -0,0 +1,17 @@
+
+exports.add = (a=1, b=1) => {
+    return a + b
+}
+
+exports.subtract = (a=1, b=1) => {
+    return a - b
+}
+exports.multiply = (a=1, b=1) => {
+    return a * b
+}
+
+exports.divide = (a=1, b=1) => {
+    return a / b
+};
+
+// module.exports = {add, subtract, multiply, divide}
