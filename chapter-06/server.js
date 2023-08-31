366f9e2610e76e207b004f5e554f7a4640d7e906
Onyeriri Victor <viktorchinonso@gmail.com>
2023-08-31 13:01:57 +0100

    Finished route handlers

--- chapter-06/server.js
+++ chapter-06/server.js
@@ -1,10 +1,41 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello world!!!');
+// chaining handlers
+const one = (req, res, next) => {
+    console.log('One');
+    next();
+}
+
+const two = (req, res, next) => {
+    console.log('Two');
+    next();
+}
+
+const three = (req, res) => {
+    console.log('Three');
+    res.send('Finished!!!');
+}
+
+app.get('/chain(.html)?', [one, two, three])
+
+app.get('^/$|/index(.html)?', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, 'new-page.html');
+});
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 app.listen(PORT, () => console.log(`App running on port ${PORT}...`));
