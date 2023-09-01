b4be7fb94f0386ed34cbda929b2cc1657b0af045
Onyeriri Victor <viktorchinonso@gmail.com>
2023-09-01 19:38:47 +0100

    Completed chapter 07

---
+++ chapter-07/middleware/logEvents.js
@@ -0,0 +1,34 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyy-MM-dd\thh:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+    try {
+        if (!fs.existsSync(path.join(__dirname,'..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname,'..', 'logs'), () => console.log('Directory created.'));
+        }
+
+        await fsPromises.appendFile(path.join(__dirname,'..', 'logs', logName), logItem, () => {
+            console.log('File append completed!!!');
+        })
+    } catch (error) {
+       console.error(error); 
+    }
+
+    console.log(logItem);
+};
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t ${req.headers.origin}\t ${req.url}`, 'reqLog.txt');
+    console.log(`${req.method}\t ${req.url}`);
+    next();
+}
+
+module.exports = {logger, logEvents}
+
