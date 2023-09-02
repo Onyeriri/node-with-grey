69a92211e163c1552d5f5a62df93c768fc006fb2
Onyeriri Victor <viktorchinonso@gmail.com>
2023-09-02 17:09:24 +0100

    Completed chapter 09

---
+++ chapter-09/sever.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const { logger } = require('./middleware/logEvents');
+const cors = require('cors');
+const errorHandler = require('./middleware/errorHandler');
+
+const corsOptions = require('./config/config');
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+// custom middleware
+app.use(logger);
+
+// cross origin resource sharing
+app.use(cors(corsOptions));
+
+// middleware for form data
+app.use(express.urlencoded({ extended: false }));
+// middleware for json files
+app.use(express.json());
+// middleware for static files
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/root'));
+app.use('/employees', require('./routes/api/employee'));
+
+app.all('*', (req, res) => {
+    res.status(404);
+
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ err: '404 Not Found' });
+    } else
+        res.type('txt').send('404 Not Found');
+});
+
+// error handler
+app.use(errorHandler);
+
+// server running on port 3500
+app.listen(PORT, () => console.log(`App listening to port ${PORT}`));
